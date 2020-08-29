cd7ab2d376588ca5ee0676c54a013e8af076b037
Rodger Gu <rgu663@aucklanduni.ac.nz>
2020-08-29 13:43:53 +1200

    Added markdown parser, quick test to see if it functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 
+const input = '# Test Header\n\n ## And a sub-header \n\n And a test paragraph\n\n And another paragraph';
+
 const App: React.FC = () => 
   <Container>
     <Row>
       <Button>Bootstrap Button</Button>
+    </Row>
+    <Row>
+      <ReactMarkdown source={input} />
     </Row>
   </Container>;
 
