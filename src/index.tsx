ddce7ac9839aaeb90a3951e2815e26be290e2b7c
Rodger Gu <rgu663@aucklanduni.ac.nz>
2020-08-30 20:25:50 +1200

    Added a scaffold for top nav. Added in files for Article and Article Container

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TopNav from './TopNav';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TopNav />
   </React.StrictMode>,
   document.getElementById('root')
 );
